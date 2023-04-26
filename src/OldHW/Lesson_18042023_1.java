1ae112b82830edcbdd0003a870a29c7db1f00b02
IgorGabrinets <gabrinets1@gmail.com>
2023-04-26 18:02:24 +0200

    Lesson_21032023_1

---
+++ src/OldHW/Lesson_18042023_1.java
@@ -0,0 +1,15 @@
+package OldHW;
+
+import java.util.function.Function;
+
+public class Lesson_18042023_1 {
+    public static void main(String[] args) {
+        Function<Integer, Integer> func = x -> x * 10;
+        func = func.andThen(x -> x + 1);
+        func = func.andThen(x -> x + 3);
+        func = func.andThen(x -> x * 2);
+
+        int result = func.apply(3);
+        System.out.println(result);
+    }
+}
